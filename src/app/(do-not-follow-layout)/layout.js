5bef4f66b5beebf50bb98577f10c22efc3b799d9
bhavik-wts <bhavik.d@wingsts.com>
2025-04-30 19:21:37 +0530

    layout update

--- src/app/(do-not-follow-layout)/layout.js
+++ src/app/(do-not-follow-layout)/layout.js
@@ -23,18 +23,17 @@ export default function ViewerLayout({ children }) {
                         {
                             "imports": {
                                 "three": "https://cdn.jsdelivr.net/npm/three@^0.167.1/build/three.module.min.js",
-                                "@google/model-viewer": "https://unpkg.com/@google/model-viewer/dist/model-viewer.min.js",
                                 "@google/model-viewer-effects": "https://cdn.jsdelivr.net/npm/@google/model-viewer-effects/dist/model-viewer-effects.min.js"
                             }
                         }
                     `}
                 </Script>
 
-                <Script
+                {/* <Script
                     type="module"
                     src="https://unpkg.com/@google/model-viewer/dist/model-viewer.min.js"
                     strategy="beforeInteractive" // Load before interactive
-                ></Script>
+                ></Script> */}
 
                 <Script
                     type="module"
